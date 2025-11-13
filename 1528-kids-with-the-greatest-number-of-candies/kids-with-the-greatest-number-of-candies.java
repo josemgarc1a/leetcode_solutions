71cc137d4743e05b9fc37d7c0aebf5619474f1e3
Jose Garcia <josemgarc1a@icloud.com>
2025-11-13 10:52:59 -0600

    Time: 1 ms (84.11%) | Memory: 44 MB (6.00%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.java
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.java
@@ -1,11 +1,14 @@
 class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
-        int maxCandies = Arrays.stream(candies).max().orElse(0);
+        int maxCandies = 0;
+        for (int kidCandies : candies) {
+            maxCandies=Math.max(kidCandies, maxCandies);
+        }
 
-        List<Boolean> results = Arrays.stream(candies)
-            .mapToObj(candy -> candy+extraCandies >= maxCandies)
-            .collect(Collectors.toList());
-        
+        List<Boolean> results = new ArrayList<>();
+        for (int kidCandies : candies) {
+            results.add(kidCandies+extraCandies >= maxCandies);
+        }
         return results;
-    } 
+    }
 }
